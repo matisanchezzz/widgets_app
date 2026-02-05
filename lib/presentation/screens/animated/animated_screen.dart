93a10b764faeb88c29cbd7f8d997d14935ce6882
Matias <matiassanchez496@hotmail.com>
2026-02-05 10:01:48 -0300

    Animated Finished

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,10 +1,37 @@
+import 'dart:math' show Random;
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const name = 'animated_screen';
   static const path = '/animated';
 
   const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double height = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 100;
+
+  void changeShape() {
+    final random = Random();
+
+    width = random.nextInt(300) + 50;
+    height = random.nextInt(300) + 50;
+    borderRadius = random.nextInt(100) + 10;
+    color = Color.fromRGBO(
+      random.nextInt(255), //red
+      random.nextInt(255), //green
+      random.nextInt(255), //blue
+      1, //opacity
+    );
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,17 +42,19 @@ class AnimatedScreen extends StatelessWidget {
         child: AnimatedContainer(
           duration: const Duration(milliseconds: 400),
           curve: Curves.easeOutCubic,
-          width: 200,
-          height: 230,
+          width: width <= 0 ? 0 : width,
+          height: height <= 0 ? 0 : height,
           decoration: BoxDecoration(
-            color: Colors.deepPurple,
-            borderRadius: BorderRadius.circular(30),
+            color: color,
+            borderRadius: BorderRadius.circular(
+              borderRadius < 0 ? 0 : borderRadius,
+            ),
           ),
         ),
       ),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: changeShape,
 
         child: const Icon(Icons.play_arrow_rounded),
       ),
