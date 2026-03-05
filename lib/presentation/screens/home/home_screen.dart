6537178f0a989d5550a46e036aaa3ef6da372656
Matias <matiassanchez496@hotmail.com>
2026-03-05 10:58:24 -0300

    NavigationDrawer - Menu Lateral

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/widgets/side_menu.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home';
@@ -13,6 +14,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Flutter + Material 3')),
       body: _HomeView(),
+      drawer: const SideMenu(),
     );
   }
 }
